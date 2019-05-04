ea3604a6582396c30f7aafc1799f5d65436ef0bd
Yuriy <Seaman_on_watch@mail.ru>
2019-05-04 19:41:33 +0300

    Creating Base class

--- src/test/java/com/w2a/base/TestBase.java
+++ src/test/java/com/w2a/base/TestBase.java
@@ -4,13 +4,45 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.openqa.selenium.WebDriver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class TestBase {
 
     public static WebDriver driver;
+    public static Properties config = new Properties();
+    public static Properties OR = new Properties();
+    public static FileInputStream fis;
 
     @BeforeSuite
-    public void setUp(){
+    public void setUp() {
 
+        if (driver==null){
+            try {
+                fis = new FileInputStream(System.getProperty("user.dir")+
+                        "\\src\\test\\resources\\properties\\Config.properties");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                config.load(fis);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fis = new FileInputStream(System.getProperty("user.dir")+
+                        "\\src\\test\\resources\\properties\\OR.properties");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                OR.load(fis);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @AfterSuite
