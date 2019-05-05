c79132f08efc96427864134fd2eeba77dfa80076
Yuriy <Seaman_on_watch@mail.ru>
2019-05-05 19:57:08 +0300

    Initializing Logs

--- src/test/java/com/w2a/base/TestBase.java
+++ src/test/java/com/w2a/base/TestBase.java
@@ -1,5 +1,6 @@
 package com.w2a.base;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -19,6 +20,7 @@ public class TestBase {
     public static Properties config = new Properties();
     public static Properties OR = new Properties();
     public static FileInputStream fis;
+    public static Logger log = Logger.getLogger("devpinoyLogger");
 
     @BeforeSuite
     public void setUp() {
@@ -32,6 +34,7 @@ public class TestBase {
             }
             try {
                 config.load(fis);
+                log.debug("Config file loaded !");
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -43,6 +46,7 @@ public class TestBase {
             }
             try {
                 OR.load(fis);
+                log.debug("OR file loaded !");
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -54,12 +58,14 @@ public class TestBase {
                 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +
                         "\\src\\test\\resources\\executables\\chromedriver.exe");
                 driver = new ChromeDriver();
+                log.debug("Chrome launched !");
             } else if (config.getProperty("browser").equals("ie")) {
                 System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") +
                         "\\src\\test\\resources\\executables\\IEDriverServer.exe");
                 driver = new InternetExplorerDriver();
             }
             driver.get(config.getProperty("testsiteurl"));
+            log.debug("Navigated to: " + config.getProperty("testsiteurl"));
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
         }
@@ -71,5 +77,6 @@ public class TestBase {
         if (driver != null) {
             driver.quit();
         }
+        log.debug("Test execution completed !");
     }
 }
