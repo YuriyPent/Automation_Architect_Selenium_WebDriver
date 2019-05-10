78f4efdaffcd277cbf06e3edbc8a8b5958e45feb
Yuriy <Seaman_on_watch@mail.ru>
2019-05-10 20:45:28 +0300

    Capturing unique screenshot and report customization

--- src/test/java/com/w2a/listeners/CustomListeners.java
+++ src/test/java/com/w2a/listeners/CustomListeners.java
@@ -1,9 +1,12 @@
 package com.w2a.listeners;
 
+import com.w2a.utilities.TestUtil;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
+
+import java.io.IOException;
 
 public class CustomListeners implements ITestListener {
 
@@ -13,17 +16,22 @@ public class CustomListeners implements ITestListener {
 
     public void onTestSuccess(ITestResult iTestResult) {
 
+        System.setProperty("org.uncommons.reportng.escape-output", "false");
+        try {
+            TestUtil.captureScreenshot();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Reporter.log("Click to see Screenshot");
+        Reporter.log("Login successfully executed");
+        Reporter.log("<a target=\"_blank\" href=" + TestUtil.screenshotName +">Screenshot</a>");
+        Reporter.log("<br>");
+        Reporter.log("<br>");
+        Reporter.log("<a target=\"_blank\" href=" + TestUtil.screenshotName +"><img src=" + TestUtil.screenshotName +
+                "height=200 width=200></img></a>");
     }
 
     public void onTestFailure(ITestResult iTestResult) {
-
-        System.setProperty("org.uncommons.reportng.escape-output", "false");
-        Reporter.log("Capturing screenshot");
-        Reporter.log("Login successfully executed");
-        Reporter.log("<a target=\"_blank\" href=\"‪src\\test\\resources\\error.jpg\">Screenshot</a>");
-        Reporter.log("<br>");
-        Reporter.log("<a target=\"_blank\" href=\"‪src\\test\\resources\\error.jpg\">img " +
-                "src=\"src\\test\\resources\\error.jpg\"></img></a>");
 
     }
 
