4a359e5d9dfc1ba78722bb1668491c29741f0089
Yuriy <Seaman_on_watch@mail.ru>
2019-06-23 21:28:38 +0300

    Add base class Page

---
+++ PageObjectModelBasics/src/main/java/com/w2a/pages/ZohoAppPage.java
@@ -0,0 +1,21 @@
+package com.w2a.pages;
+
+import com.w2a.base.Page;
+import com.w2a.pages.crm.CRMHomePage;
+import org.openqa.selenium.By;
+
+public class ZohoAppPage extends Page {
+
+    public void gotoChat() {
+        driver.findElement(By.cssSelector(".zicon-apps-chat.zicon-apps-96")).click();
+    }
+
+    public CRMHomePage gotoCRM() {
+        click("crmlink_CSS");
+        return new CRMHomePage();
+    }
+
+    public void gotoSalesIQ() {
+        driver.findElement(By.cssSelector(".zicon-apps-salesiq.zicon-apps-96")).click();
+    }
+}
